b9c79a7a454b33e5864d5889ccf3a76d8384464c
Tobias-Secher <tobias.secher@gmail.com>
2013-12-18 11:01:21 +0100

    Tower Class

---
+++ TowerDefense/TowerDefense/Tower.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TowerDefense
+{
+    public abstract class Tower
+    {
+        // Field
+        private Stopwatch stopWatch;
+        private int speed;
+        private int damage;
+        private int cost;
+        private string sprite;
+        // Property
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+        public int Damage
+        {
+            get { return damage; }
+        }
+        /// <summary>
+        /// The COnstructor for the Tower Class
+        /// </summary>
+        public Tower(int speed, int damage, int cost)
+        {
+
+        }
+    }
+}
