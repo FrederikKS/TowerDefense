b7e2d2da45a001f8acf777721aa3eada40a628e3
Tobias-Secher <tobias.secher@gmail.com>
2013-12-18 12:27:54 +0100

    TowerSlow : Tower

---
+++ TowerDefense/TowerDefense/TowerSlow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TowerDefense
+{
+    public class TowerSlow : TowerType
+    {
+        // Field
+        private float slow;
+        private int range;
+        // Property
+        public float Slow
+        {
+            get { return slow; }
+        }
+        public int Range
+        {
+            get { return range; }
+        }
+
+        public TowerSlow(float slow, int range, int speed, int damage, int cost, string imagePath, PointF position, bool isClickAble)
+            : base(speed, damage, cost, range, imagePath, position, isClickAble)
+        {
+
+        }
+    }
+}
