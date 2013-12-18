472dde8725ceb913b41ae23ee5541545953a9509
Tobias-Secher <tobias.secher@gmail.com>
2013-12-18 22:49:42 +0100

    GUI first example

--- TowerDefense/TowerDefense/Form1.cs
+++ TowerDefense/TowerDefense/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Drawing;
 
 namespace TowerDefense
 {
@@ -22,11 +23,11 @@ namespace TowerDefense
             InitializeComponent();
             timer1.Enabled = true;
             timer1.Interval = 40;
-            pnl_Main.Visible = true;
+            pnl_Main.Visible = false;
             pnl_dif.Visible = false;
             pnl_high.Visible = false;
             pnl_submit.Visible = false;
-            difc = 0;
+            MouseUp += new MouseEventHandler(Form1_MouseUp);
         }
         #region Difficulty
         private void btn_dif_Click(object sender, EventArgs e)
@@ -143,5 +144,59 @@ namespace TowerDefense
                 Form1.ActiveForm.Close();
             }
         }
+        int x = 0, y = 0;
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            Graphics g = CreateGraphics();
+            //Pen p = new Pen(Color.Navy);
+            //Pen erase = new Pen(Color.White);
+            //g.DrawLine(erase, 0,0,x,y);
+            x = e.X;
+            y = e.Y;
+            //g.DrawLine(p, 0, 0, x, y);
+            //lblX.Location = new Point(x - lblX.Width, y);
+            //lblY.Location = new Point(x, y - lblY.Height);
+        }
+        void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+
+            if (e.Button == MouseButtons.Left)
+            {
+                // Test
+                
+                // Tower 1
+                Point point1 = new Point(x, y);
+                Point point2 = new Point(x, y + 150);
+                // Tower 2
+                Point point3 = new Point(x, y);
+                Point point4 = new Point(x - 120, y - 90);
+                // Tower 3
+                Point point5 = new Point(x, y);
+                Point point6 = new Point(x + 120, y - 90);
+                // Pen
+                Pen p = new Pen(Color.Red);
+                Pen pp = new Pen(Color.Blue);
+                Pen ppp = new Pen(Color.Green);
+                // The Large Ellipse
+                g.DrawEllipse(p, x - 150, y - 150, 300, 300);
+                // Drawing the lines
+                g.DrawLine(p, point1, point2);
+                g.DrawLine(pp, point3, point4);
+                g.DrawLine(ppp, point5, point6);
+                // The Small Ellipse
+                g.DrawEllipse(p, x - 50, y - 50, 100, 100);
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Image
+            Image myImg = Image.FromFile(@"Images/tiel.png");
+            e.Graphics.DrawImage(myImg, 10, 10, myImg.Width, myImg.Height);
+            base.OnPaint(e);
+        }
+
     }
 }
