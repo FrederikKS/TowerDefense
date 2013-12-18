b6dd1dcce5727f6c7662070a376573a3fd8a238a
Tobias-Secher <tobias.secher@gmail.com>
2013-12-18 13:16:55 +0100

    GameWorld Now With Summary

--- TowerDefense/TowerDefense/GameWorld.cs
+++ TowerDefense/TowerDefense/GameWorld.cs
@@ -60,56 +60,96 @@ namespace TowerDefense
 
         // Functions
 
+        /// <summary>
+        /// Draws The World
+        /// </summary>
         public void SetupWorld()
         {
 
         }
-
+        /// <summary>
+        /// Makes sure all the update functions is called every frames
+        /// </summary>
         public void GameLoop()
         {
 
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
         public void Update()
         {
 
         }
-
+        /// <summary>
+        /// Draws The World
+        /// </summary>
         public void Draw()
         {
 
         }
-
+        /// <summary>
+        /// Checking If The Location Of The Treasure Island Is On A Valid Location
+        /// </summary>
+        /// <param name="startX"></param>
+        /// <param name="startY"></param>
+        /// <param name="endX"></param>
+        /// <param name="endY"></param>
+        /// <returns></returns>
         public bool CheckLocation(int startX, int startY, int endX, int endY)
         {
             return true;
         }
-
+        /// <summary>
+        /// Checking If The Location Of The Treasure Island Is On A Valid Location And Is Minimum Px From Start
+        /// </summary>
+        /// <param name="startX"></param>
+        /// <param name="startY"></param>
+        /// <param name="endX"></param>
+        /// <param name="endY"></param>
+        /// <returns></returns>
         public bool CheckLocation(int startX, int startY, int endX, int endY, int minDistStart)
         {
             return true;
         }
-
+        /// <summary>
+        /// Builds The Road The Enemies Will Follow
+        /// </summary>
+        /// <param name="worldSizeX"></param>
+        /// <param name="worldSizeY"></param>
+        /// <param name="startX"></param>
+        /// <param name="startY"></param>
+        /// <param name="endX"></param>
+        /// <param name="endY"></param>
+        /// <param name="coordinateSystem"></param>
         public void RoadBuilder(float worldSizeX, float worldSizeY, int startX, int startY, int endX, int endY, int[][] coordinateSystem)
         {
 
         }
-
+        /// <summary>
+        /// Checks What Wave The Player Is On
+        /// </summary>
         public void GameState()
         {
 
         }
-
+        /// <summary>
+        /// The Build Function
+        /// </summary>
         public void Build()
         {
 
         }
-
+        /// <summary>
+        /// The Sell Function
+        /// </summary>
         public void Sell()
         {
 
         }
-
+        /// <summary>
+        /// Checking If The Enemies Are On A Checkpoing
+        /// </summary>
         public void OnCheckpoint()
         {
 
