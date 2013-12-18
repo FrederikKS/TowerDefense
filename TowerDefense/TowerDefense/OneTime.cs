0da8f56e81ea94335613b6f005ed0c7329525d06
Tobias-Secher <tobias.secher@gmail.com>
2013-12-18 12:44:11 +0100

    OnTime

---
+++ TowerDefense/TowerDefense/OneTime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TowerDefense
+{
+    public class OneTime : Tower
+    {
+        /// <summary>
+        /// Constructor For The OneTime Use Towers
+        /// </summary>
+        /// <param name="speed"></param>
+        /// <param name="damage"></param>
+        /// <param name="cost"></param>
+        /// <param name="ranged"></param>
+        /// <param name="imagePath"></param>
+        /// <param name="position"></param>
+        /// <param name="isClickAble"></param>
+        public OneTime(float speed, int damage, int cost, float ranged, string imagePath, PointF position, bool isClickAble) 
+            : base(speed,damage,cost, ranged, imagePath,position,isClickAble)
+        {
+
+        }
+    }
+}
