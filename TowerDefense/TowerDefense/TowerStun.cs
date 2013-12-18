607a6ddbed009dc9764ca16d3ee7ab3469b4a3f1
Tobias-Secher <tobias.secher@gmail.com>
2013-12-18 12:39:04 +0100

    TowerStun

---
+++ TowerDefense/TowerDefense/TowerStun.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TowerDefense
+{
+    public class TowerStun : TowerType
+    {
+        // Fields
+        private float stunTime;
+        // Property
+        public float StunTime
+        {
+            get {return stunTime; }
+        }
+        /// <summary>
+        /// The Constructor For The TowerStun
+        /// </summary>
+        /// <param name="stunTime"></param>
+        /// <param name="speed"></param>
+        /// <param name="damage"></param>
+        /// <param name="cost"></param>
+        /// <param name="range"></param>
+        /// <param name="imagePath"></param>
+        /// <param name="position"></param>
+        /// <param name="isClickAble"></param>
+        public TowerStun(float stunTime,float speed, int damage, int cost, float range, string imagePath, PointF position, bool isClickAble)
+            : base(speed, damage, cost, range, imagePath, position, isClickAble)
+        {
+
+        }
+    }
+}
