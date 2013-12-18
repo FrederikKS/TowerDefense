cbf677004c8d34f209d5b6510f4647f314a81337
Tobias-Secher <tobias.secher@gmail.com>
2013-12-18 11:53:03 +0100

    TowerType Class

---
+++ TowerDefense/TowerDefense/TowerType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TowerDefense
+{
+    public abstract class TowerType : Tower
+    {
+        /// <summary>
+        /// Constuctor For TowerTypes
+        /// </summary>
+        /// <param name="speed"></param>
+        /// <param name="damage"></param>
+        /// <param name="cost"></param>
+        /// <param name="range"></param>
+        /// <param name="imagePath"></param>
+        /// <param name="position"></param>
+        /// <param name="isClickAble"></param>
+        public TowerType(int speed, int damage, int cost, float range, string imagePath, PointF position, bool isClickAble) : base(speed, damage, cost, range, imagePath, position, isClickAble)
+        {
+
+        }
+    }
+}
