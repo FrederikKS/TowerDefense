97348ba6e4261c155c64d07d8584f802db0c26a1
FrederikKS <frederik.svendsen@gmail.com>
2014-01-13 12:48:11 +0100

    Fixed cannonball hitbox

--- TowerDefense/TowerDefense/Projectile.cs
+++ TowerDefense/TowerDefense/Projectile.cs
@@ -123,7 +123,7 @@ namespace TowerDefense
 
                     position = new PointF(position.X - Math.Abs(direction.X * correctionX), position.Y - Math.Abs(direction.Y * correctionY));
                 }
-                if (target.Position.X + Form1.gw.offsetX >= position.X + Form1.gw.offsetX && position.X + Form1.gw.offsetX <= target.Position.X + Form1.gw.offsetX + 90 && target.Position.Y + Form1.gw.offsetY <= position.Y + Form1.gw.offsetY && position.Y + Form1.gw.offsetY <= target.Position.Y + Form1.gw.offsetY + 90)
+                if (target.Position.X + Form1.gw.offsetX >= position.X + Form1.gw.offsetX && position.X + Form1.gw.offsetX <= target.Position.X + Form1.gw.offsetX && target.Position.Y + Form1.gw.offsetY <= position.Y + Form1.gw.offsetY && position.Y + Form1.gw.offsetY <= target.Position.Y + Form1.gw.offsetY)
                 {
                     if (damage > 0)
                     {
